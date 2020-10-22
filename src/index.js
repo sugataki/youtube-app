4a19ae4095b08c988045ebedfb9b8d12335ee04e
sugataki <sugataki1010@gmail.com>
2020-10-22 21:35:07 +0900

    chap7 add global state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import StoreProvider from "./store/index.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StoreProvider>
+    <App />
+  </StoreProvider>,
+  document.getElementById("root")
+);
--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import Layout from "../components/Layout/Layout";
 import { fetchPopularData } from "../apis/index";
+import { Store } from "../store/index";
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store);
+
   useEffect(() => {
     fetchPopularData().then((res) => {
       console.log(res);
+      setGlobalState({
+        type: "SET_POPULAR",
+        payload: { popular: res.data.items },
+      });
     });
   }, []);
   return <Layout>Top page</Layout>;
