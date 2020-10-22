326a84e1f70046a62a6ddfb859f106215cdcb671
sugataki <sugataki1010@gmail.com>
2020-10-22 20:40:13 +0900

    chap4 layout header components

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Layout from "../components/Layout/Layout";
 
 const Top = () => {
-  return <div>Top page</div>;
+  return <Layout>Top page</Layout>;
 };
 
 export default Top;
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Layout from "../components/Layout/Layout";
 
 const Watch = () => {
-  return <div>Watch page</div>;
+  return <Layout>Watch page</Layout>;
 };
 
 export default Watch;
