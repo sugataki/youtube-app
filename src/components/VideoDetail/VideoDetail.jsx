929706a7faac0f7d6366b29741f6bd9b6a997126
sugataki <sugataki1010@gmail.com>
2020-10-25 07:48:49 +0900

    chap9 get video details

---
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { fetchSelectedData } from "../../apis/index";
+import { Store } from "../../store/index";
+
+const VideoDetail = () => {
+  const { globalState, setGlobalState } = useContext(Store);
+  const location = useLocation();
+  const setSelectedVideo = async () => {
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get("v");
+    await fetchSelectedData(id).then((res) => {
+      const item = res.data.items.shift();
+      console.log(item);
+      setGlobalState({ type: "SET_SELECTED", payload: { selected: item } });
+    });
+  };
+
+  useEffect(() => {
+    setSelectedVideo();
+  }, []);
+  return <div></div>;
+};
+
+export default VideoDetail;
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import VideoDetail from "../components/VideoDetail/VideoDetail";
 
 const Watch = () => {
-  return <Layout>Watch page</Layout>;
+  return (
+    <Layout>
+      Watch page
+      <VideoDetail />
+    </Layout>
+  );
 };
 
 export default Watch;
