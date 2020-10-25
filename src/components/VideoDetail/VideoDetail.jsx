1787dc5c728b578ae985b0cf673f0a63de9cdc7f
sugataki <sugataki1010@gmail.com>
2020-10-25 13:12:44 +0900

    chap12 optimize details page

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -1,27 +1,11 @@
 import React, { useEffect, useContext } from "react";
-import { useLocation } from "react-router-dom";
-import { fetchSelectedData } from "../../apis/index";
 import { Store } from "../../store/index";
 import VideoPlay from "../VideoPlay/VideoPlay";
 import Style from "./VideoDetail.module.scss";
 import Linkify from "react-linkify";
 
 const VideoDetail = () => {
-  const { globalState, setGlobalState } = useContext(Store);
-  const location = useLocation();
-  const setSelectedVideo = async () => {
-    const searchParams = new URLSearchParams(location.search);
-    const id = searchParams.get("v");
-    await fetchSelectedData(id).then((res) => {
-      const item = res.data.items.shift();
-      console.log(item);
-      setGlobalState({ type: "SET_SELECTED", payload: { selected: item } });
-    });
-  };
-
-  useEffect(() => {
-    setSelectedVideo();
-  }, [location.search]);
+  const { globalState } = useContext(Store);
 
   return globalState.selected && globalState.selected.id ? (
     <div className={Style.wrap}>
